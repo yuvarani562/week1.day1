18e1b7d871e8a0c541719357cd30d370a91dcb0f
yuvarani562 <107866414+yuvarani562@users.noreply.github.com>
2022-06-20 21:41:21 +0530

    Add files via upload
    week1 day2 Assignment

---
+++ Twowheeler.java
@@ -0,0 +1,30 @@
+package week1day2ass;
+
+public class Twowheeler {
+
+	 int numberofwheels = 8;
+	 short numberofmirrors = 2;
+	 long chassisnumber = 433333333;
+	 String bikename = "honda";   
+	 double runningKM =9864.34454;
+	 boolean isbike = true;
+	 
+	 
+	 
+	 public static void main(String[] args) {
+
+		// classname objname = new classname();
+		 Twowheeler twowheelerObj = new Twowheeler();
+		 System.out.println("numberofwheels : "+twowheelerObj.numberofwheels );
+		 
+	    System.out.println("numberofmirrors:"+twowheelerObj.numberofmirrors);
+		System.out.println("chassisnumber:"+twowheelerObj.chassisnumber);
+		System.out.println("String bikename:"+twowheelerObj.bikename);
+		 System.out.println("double runningKM:"+twowheelerObj.runningKM);
+		 System.out.println("boolean isbike:"+twowheelerObj.isbike);
+		 
+	 }
+		  
+	 }	
+  
+
