285481e0056daf2a561ad5d9252629011cae42dc
yuvarani562 <107866414+yuvarani562@users.noreply.github.com>
2022-06-20 21:43:45 +0530

    Add files via upload
    week1 day1 assignment

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class MyFirstJavaProgram {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		System.out.println("Welcome to Testleaf");
+		
+	float a=5.5f;
+		int b=7;
+		float c=a+b;
+		System.out.println(a+b);
+		System.out.println("c values "+c);
+				
+	}
+
+}
